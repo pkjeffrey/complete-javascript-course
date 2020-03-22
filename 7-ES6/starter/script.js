9f24c2f473ff5aac4455153a7136ad16946e4994
Peter <pkjeffrey>
2020-03-22 15:18:26 +1030

    ES6 6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -357,7 +357,7 @@ var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
 
 
 // maps //////////////////////////////////////////////////
-
+/*
 const question = new Map();
 question.set('question', 'What is the official name of the latest major JavaScript version?');
 question.set(1, 'ES5');
@@ -388,3 +388,90 @@ for (let [key, value] of question.entries()) {
 const ans = parseInt(prompt('Write the correct answer'));
 
 console.log(question.get(question.get('correct') === ans));
+*/
+
+
+// classes ////////////////////////////////////////////////
+/*
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+Person5.prototype.calcAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
+
+// ES6
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    calcAge() {
+        const age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+    static greeting() {
+        console.log('Hello.');
+    }
+}
+
+const john6 = new Person6('John', 1990, 'teacher');
+Person6.greeting();
+*/
+
+
+// inheritance ///////////////////////////////////////////
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+Person5.prototype.calcAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+}
+Athlete5.prototype = Object.create(Person5.prototype);
+Athlete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log(this.medals);
+}
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+// ES6
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    calcAge() {
+        const age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
