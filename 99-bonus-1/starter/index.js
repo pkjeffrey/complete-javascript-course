fba73c06e6f99825b299521dd107fb00a76b28d3
P.Jeffrey <peterk.jeffrey@sa.gov.au>
2020-07-02 15:51:50 +0930

    99 complete

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -16,15 +16,55 @@ function requestHandler(request, response) {
     const query = requestURL.query;
 
     if (pathname === '/' || pathname === '/products') {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end('products page');
+        renderOverview(response);
 
     } else if (pathname === '/laptop' && query.id >= 0 && query.id < laptopData.length) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(`laptop page for ${query.id}`);
+        renderLaptop(laptopData[query.id], response);
 
+    } else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathname)) {
+        serveImage(pathname, response);
+        
     } else {
         response.writeHead(404, {'Content-Type': 'text/html'});
         response.end('not found');
     }
+}
+
+function serveImage(image, response) {
+    fs.readFile(`./data/img${image}`, (err, data) => {
+        response.writeHead(200, {'Content-Type': 'image/jpg'});
+        response.end(data);
+    });
+}
+
+function renderOverview(response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./templates/overview.html', 'utf-8', (err, data) => {
+        let output = data;
+        fs.readFile('./templates/cards.html', 'utf-8', (err, data) => {
+            const cards = laptopData.map(el => replaceData(data, el)).join('');
+            output = output.replace('{%CARDS%}', cards);
+            response.end(output);
+        });
+    });
+}
+
+function renderLaptop(laptop, response) {
+    fs.readFile('./templates/laptop.html', 'utf-8', (err, data) => {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        let output = replaceData(data, laptop);
+        response.end(output);
+    });
+}
+
+function replaceData(template, laptop) {
+    return template.replace(/{%PRODUCTNAME%}/g, laptop.productName)
+        .replace(/{%IMAGE%}/g, laptop.image)
+        .replace(/{%PRICE%}/g, laptop.price)
+        .replace(/{%SCREEN%}/g, laptop.screen)
+        .replace(/{%CPU%}/g, laptop.cpu)
+        .replace(/{%RAM%}/g, laptop.ram)
+        .replace(/{%STORAGE%}/g, laptop.storage)
+        .replace(/{%DESCRIPTION%}/g, laptop.description)
+        .replace(/{%ID%}/g, laptop.id);
 }
